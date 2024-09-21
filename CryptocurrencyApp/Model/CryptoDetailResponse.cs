8674b9f799f28fc3208f51f30333991cb6573066
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-21 17:36:37 +0300

    add CryptoDataDetail.cs & CryptoDetailResponse

---
+++ CryptocurrencyApp/Model/CryptoDetailResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyApp.Model
+{
+    public class CryptoDetailResponse
+    {
+        public List<CryptoDataDetail> Data { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
