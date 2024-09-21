2a37edf704341263de81919d18a29d41f1c117dc
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-21 20:16:37 +0300

    add converter

---
+++ CryptocurrencyApp/Converters/BoolToRefreshTextConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CryptocurrencyApp.Converters
+{
+    public class BoolToRefreshTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isLoading = (bool)value;
+            if (isLoading)
+            {
+                return "Refreshing...";
+            }
+            else
+            {
+                return "Refresh";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
