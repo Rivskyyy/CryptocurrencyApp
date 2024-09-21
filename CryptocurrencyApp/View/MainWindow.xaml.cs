4f13f5ccdfc9d6e765c2b2d63ceb1ea0f95e909b
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-21 20:19:37 +0300

    add DataGrid.Columns to MainView

--- CryptocurrencyApp/View/MainWindow.xaml.cs
+++ CryptocurrencyApp/View/MainWindow.xaml.cs
@@ -1,22 +1,15 @@
 ﻿using CryptocurrencyApp.ViewModel;
-using System.Diagnostics;
 using System.Windows;
 
 namespace CryptocurrencyApp.View
 {
-
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
-
-           
             MainWindowViewModel viewModel = new MainWindowViewModel();
-        
-            this.DataContext = viewModel;
-            
+            DataContext = viewModel;
         }
     }
 }
