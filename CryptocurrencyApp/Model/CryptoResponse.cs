f3bf516375f4a6109717698b7bcb267f511e7d79
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-21 17:38:01 +0300

    Refactor CryptoData & CryptoResponse

--- CryptocurrencyApp/Model/CryptoResponse.cs
+++ CryptocurrencyApp/Model/CryptoResponse.cs
@@ -8,9 +8,7 @@ namespace CryptocurrencyApp.Model
 {
     public class CryptoResponse
     {
-
         public List<CryptoData> Data { get; set; }
         public long Timestamp { get; set; }
-
     }
 }
