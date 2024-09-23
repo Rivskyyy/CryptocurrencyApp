dd61e6210ad7cee2e90597ef6412643e95b84467
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-23 13:11:50 +0300

    Add DetailWindowViewModel

---
+++ CryptocurrencyApp/ViewModel/DetailsWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CryptocurrencyApp.APIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyApp.ViewModel
+{
+    internal class DetailsWindowViewModel : BaseViewModel
+    {
+        private readonly CoinCapApiClient _coinCapApiClient = new CoinCapApiClient();
+        private readonly HttpClient _httpClient;
+        public Command AddCommand { get; }
+
+    }
+}
