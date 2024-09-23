74175d2585fbe9b4dbcf628ee3b30c61e91cc3df
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-23 20:57:01 +0300

    Add NavigationService.cs

---
+++ CryptocurrencyApp/Navigation/NavigationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CryptocurrencyApp.Navigation
+{
+    class NavigationService
+    {
+        private readonly Frame _frame;
+
+        public NavigationService(Frame frame)
+        {
+            _frame = frame;
+        }
+
+        public void NavigateTo<TView>() where TView : Page, new()
+        {
+            _frame.Navigate(typeof(TView));
+        }
+
+    }
+}
