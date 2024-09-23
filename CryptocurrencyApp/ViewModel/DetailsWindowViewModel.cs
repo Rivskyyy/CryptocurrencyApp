63e4f3795eb07c6f36f787b85c7a6f45362b9fc4
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-23 20:58:07 +0300

    Add LoadDetailDataAsync to DetailsWindowViewModel

--- CryptocurrencyApp/ViewModel/DetailsWindowViewModel.cs
+++ CryptocurrencyApp/ViewModel/DetailsWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CryptocurrencyApp.APIs;
+using CryptocurrencyApp.Model;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -14,7 +16,27 @@ namespace CryptocurrencyApp.ViewModel
     {
         private readonly CoinCapApiClient _coinCapApiClient = new CoinCapApiClient();
         private readonly HttpClient _httpClient;
+        private readonly string _id;
+
+        private CryptoDataDetail _cryptoDataDetail;
+
+        public CryptoDataDetail CryptoDataDetail
+        {
+            get => _cryptoDataDetail;
+            set => SetProperty(ref _cryptoDataDetail, value);
+        }
         public ICommand AddCommand { get; }
 
+        public DetailsWindowViewModel(string id)
+        {
+            _id = id;
+            LoadDetailsDataAsync();
+        }
+
+        private async void LoadDetailsDataAsync()
+        {
+            var result = await _coinCapApiClient.GetCurrencyDetailsAsync(_id);
+            CryptoDataDetail = result; 
+        }
     }
 }
