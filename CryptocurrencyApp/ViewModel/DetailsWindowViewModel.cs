4d904087c19b04aa372856668b76552977be00d9
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-26 00:29:43 +0300

    Add btnBack

--- CryptocurrencyApp/ViewModel/DetailsWindowViewModel.cs
+++ CryptocurrencyApp/ViewModel/DetailsWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CryptocurrencyApp.APIs;
 using CryptocurrencyApp.Model;
+using CryptocurrencyApp.View;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows;
+using Prism.Commands;
 
 namespace CryptocurrencyApp.ViewModel
 {
@@ -17,8 +20,8 @@ namespace CryptocurrencyApp.ViewModel
         private readonly CoinCapApiClient _coinCapApiClient = new CoinCapApiClient();
         private readonly HttpClient _httpClient;
         private readonly string _id;
-
         private CryptoDataDetail _cryptoDataDetail;
+        public ICommand NavigateToHomeCommand { get; set; }
 
         public CryptoDataDetail CryptoDataDetail
         {
@@ -30,12 +33,20 @@ namespace CryptocurrencyApp.ViewModel
         {
             _id = id;
             LoadDetailsDataAsync();
+            NavigateToHomeCommand = new DelegateCommand(NavigateToHome);
         }
 
         private async void LoadDetailsDataAsync()
         {
             var result = await _coinCapApiClient.GetCurrencyDetailsAsync(_id);
             CryptoDataDetail = result; 
+        }
+
+        private void NavigateToHome()
+        {
+            var homePage = new HomePage();
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow.MainFrame.Navigate(homePage);
         }
     }
 }
