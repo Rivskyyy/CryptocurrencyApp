1bee11a2923dc39bcc74828ee85a84a77720cfdb
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-27 02:07:51 +0300

    Add MarketDataDetail

---
+++ CryptocurrencyApp/Model/MarketDataDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyApp.Model
+{
+    public class MarketDataDetail
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        public decimal PercentTotalVolume { get; set; }
+        public decimal VolumeUsd { get; set; }
+        public int TradingPairs { get; set; }
+        public bool Socket { get; set; }
+        public string ExchangeUrl { get; set; }
+        public long Updated { get; set; }
+    }
+}
