d0491a67b5aac6ba3c9f4b4a69b49f946b2beb7e
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-27 02:07:40 +0300

    Add CombinedCryptoMarketData

---
+++ CryptocurrencyApp/Model/CombinedCryptoMarketData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyApp.Model
+{
+    public class CombinedCryptoMarketData
+    {
+        public string ExchangeId { get; set; }
+        public string PriceUsd { get; set; }
+        public string ExchangeUrl { get; set; }
+    }
+}
