c2ed782d16484a6e26a50a744fe5f3220027e681
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-26 20:21:39 +0300

    Add CryptoMarketsResponse

---
+++ CryptocurrencyApp/Model/CryptoMarketsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptocurrencyApp.Model
+{
+    public class CryptoMarketsResponse
+    {
+        public List<CryptoMarkets> Data { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
