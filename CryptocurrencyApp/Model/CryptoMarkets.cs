c34ad08ab9663148a1fc0808c118f6e8130fff66
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-09-27 12:58:08 +0300

    Refactor Model

--- CryptocurrencyApp/Model/CryptoMarkets.cs
+++ CryptocurrencyApp/Model/CryptoMarkets.cs
@@ -9,18 +9,6 @@ namespace CryptocurrencyApp.Model
     public class CryptoMarkets
     {
         public string ExchangeId { get; set; }
-        public string BaseId { get; set; }
-        public string QuoteId { get; set; }
-        public string BaseSymbol { get; set; }
-        public string QuoteSymbol { get; set; }
-        public decimal VolumeUsd24Hr { get; set; }
-        public decimal PriceUsd { get; set; }
-        public decimal VolumePercent { get; set; }
-    }
-
-    public class CryptoMarkets2
-    {
-        public string ExchangeId { get; set; }
         public string PriceUsd { get; set; }
     }
 }
